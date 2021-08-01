f3062ed377c8de1752048f11527614c2c2e078e4
WelkinU <welkinuttaro@gmail.com>
2021-08-01 14:20:09 -0400

    Return format is now bbox instead of normalized bbox, migrating the image decode/encode from PIL to OpenCV - avoids the PIL fonts issue, general code cleanup, removed Pydantic validation stuff - didn't feel like it added much, people can do their own validation

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Request, Form, File, UploadFile
-from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import BaseModel
 from typing import List, Optional
 
-from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO
+import cv2
+import numpy as np
 
 import torch
 import base64
@@ -15,15 +13,15 @@ import random
 app = FastAPI()
 templates = Jinja2Templates(directory = 'templates')
 
-#so we can read main.css
-app.mount("/templates", StaticFiles(directory="templates"), name="templates")
-
 #for bbox plotting
 colors = [tuple([random.randint(0, 255) for _ in range(3)]) for _ in range(100)]
 
 model_selection_options = ['yolov5s','yolov5m','yolov5l','yolov5x']
-model_dict = {model_name: None for model_name in model_selection_options}
+model_dict = {model_name: None for model_name in model_selection_options} #set up model cache
 
+##############################################
+#-------------GET Request Routes--------------
+##############################################
 @app.get("/")
 def home(request: Request):
 	'''
@@ -36,78 +34,19 @@ def home(request: Request):
 		})
 
 
-def results_to_json(results, model):
-	''' Converts yolo model output to json (list of list of dicts)
+@app.get("/about/")
+def about_us(request: Request):
 	'''
-	return [
-				[
-					{
-					"class": int(pred[5]),
-					"class_name": model.model.names[int(pred[5])],
-					"normalized_box": pred[:4].tolist(),
-					"confidence": float(pred[4]),
-					}
-				for pred in result
-				]
-			for result in results.xyxyn
-			]
-
-
-def plot_one_box(xyxy, img, color = (255,255,255), label=None, line_thickness=None):
-	#function based on yolov5/utils/plots.py plot_one_box()
-	#implemented using PIL instead of OpenCV to avoid converting between PIL and OpenCV formats
-
-	color = color or [random.randint(0, 255) for _ in range(3)]
-	width, height = img.size
-
-	xyxy = [int(width * xyxy[0]),
-			int(height * xyxy[1]),
-			int(width * xyxy[2]),
-			int(height * xyxy[3]),
-		]
-	
-	draw = ImageDraw.Draw(img)
-
-	draw.rectangle(xyxy, outline=color, width = 3)
-
-	if label:
-		#drawing text in PIL is much harder than OpenCV due to needing ImageFont class
-		#for some reason PIL doesn't have a default font that scales...
-		try:
-			#works on Windows
-			fnt = ImageFont.truetype("arial.ttf", 36)
-		except:
-			'''
-			linux might have issues with the above font, so adding this section to handle it
-			this method is untested. based on:
-			https://stackoverflow.com/questions/24085996/how-i-can-load-a-font-file-with-pil-imagefont-truetype-without-specifying-the-ab
-			'''
-			fnt = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 36, encoding="unic")
-
-		txt_width, txt_height = fnt.getsize(label)
-
-		draw.rectangle([xyxy[0],xyxy[1]-txt_height-2, xyxy[0]+txt_width+2, xyxy[1]], fill = color)
-		draw.text((xyxy[0],xyxy[1]-txt_height), label, fill=(255,255,255), font = fnt)
-
-
-class YOLORequest(BaseModel):
-	''' Class used for pydantic validation 
-	Documentation: https://pydantic-docs.helpmanual.io/usage/validators/
+	Display about us page
 	'''
-	model_name: str
-	img_size: int
 
-	@validator('model_name')
-	def validate_model_name(cls, v):
-		assert v in model_selection_options, f'Invalid model name. Valid options: {model_selection_options}'
-		return v
-
-	@validator('img_size')
-	def validate_img_size(cls, v):
-		assert v%32 == 0 and v>0, f'Invalid inference size. Must be multiple of 32 and greater than 0.'
-		return v	
+	return templates.TemplateResponse('about.html', 
+			{"request": request})
 
 
+##############################################
+#------------POST Request Routes--------------
+##############################################
 @app.post("/")
 async def detect_via_web_form(request: Request,
 							file_list: List[UploadFile] = File(...), 
@@ -120,36 +59,16 @@ async def detect_via_web_form(request: Request,
 	Returns: HTML template render showing bbox data and base64 encoded image
 	'''
 
-	''' Notes on Pydantic validation:
-	Objective here:
-	1. Use a response model that works on multipart form data
-	2. Use Pydantic to validate the data with @validators
-	3. Don't throw an uncaught Validation error from Pydantic that causes 
-		Internal Server Error response (error code 500)
-
-	Pydantic Response models using several methods from below link don't work because data is multipart form data:
-	https://github.com/tiangolo/fastapi/issues/1989
-	multipart form data doesn't work with Pydantic per: 
-	https://github.com/tiangolo/fastapi/issues/285#issuecomment-498368734
-
-	The try-except method below was the only way I found that could accomplish all the above.
-	'''
-	try:
-		yr = YOLORequest(model_name = model_name, img_size = img_size)		
-	except ValidationError as e:
-		return JSONResponse(content=e.errors(), status_code = 422)
-
 	#assume input validated properly if we got here
 	if model_dict[model_name] is None:
 		model_dict[model_name] = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
 
-	img_batch = []
-	for i, file in enumerate(file_list):
-		img = Image.open(BytesIO(await file.read()))
-		img.filename = f'image{i}.jpg' #for https://github.com/WelkinU/yolov5-fastapi-demo/issues/5
-		img_batch.append(img)
+	img_batch = [cv2.imdecode(np.fromstring(await file.read(), np.uint8), cv2.IMREAD_COLOR)
+					for file in file_list]
 
-	results = model_dict[model_name](img_batch.copy(), size = img_size) #get YOLO results on the input image
+	#.copy() because the images are modified when running model, and we need originals when drawing bboxes later
+	results = model_dict[model_name](img_batch.copy(), size = img_size)
+
 	json_results = results_to_json(results,model_dict[model_name])
 
 	img_str_list = []
@@ -157,20 +76,17 @@ async def detect_via_web_form(request: Request,
 	for img, bbox_list in zip(img_batch, json_results):
 		for bbox in bbox_list:
 			label = f'{bbox["class_name"]} {bbox["confidence"]:.2f}'
-			plot_one_box(bbox['normalized_box'], img, label=label, 
+			plot_one_box(bbox['bbox'], img, label=label, 
 					color=colors[int(bbox['class'])], line_thickness=3)
 
-		#base64 encode the image so we can render it in HTML
-		buffered = BytesIO()
-		img.save(buffered, format="JPEG")
-		img_str_list.append(base64.b64encode(buffered.getvalue()).decode('utf-8'))
+		img_str_list.append(base64EncodeImage(img))
 
 	#escape the apostrophes in the json string representation
 	encoded_json_results = str(json_results).replace("'",r"\'").replace('"',r'\"')
 
 	return templates.TemplateResponse('show_results.html', {
 			'request': request,
-			'bbox_image_data_zipped': zip(img_str_list,json_results), #zip here, instead of in jinja2 template
+			'bbox_image_data_zipped': zip(img_str_list,json_results), #unzipped in jinja2 template
 			'bbox_data_str': encoded_json_results,
 		})
 
@@ -192,37 +108,25 @@ async def detect_via_api(request: Request,
 
 	Intended for API usage.
 	'''
-	try:
-		yr = YOLORequest(model_name = model_name, img_size = img_size)
-	except ValidationError as e:
-		return JSONResponse(content=e.errors(), status_code = 422)
 
 	if model_dict[model_name] is None:
 		model_dict[model_name] = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
 
-	img_batch = []
-	for i, file in enumerate(file_list):
-		img = Image.open(BytesIO(await file.read()))
-		img.filename = f'image{i}.jpg' #for https://github.com/WelkinU/yolov5-fastapi-demo/issues/5
-		img_batch.append(img)
+	img_batch = [cv2.imdecode(np.fromstring(await file.read(), np.uint8), cv2.IMREAD_COLOR)
+					for file in file_list]
 	
 	if download_image:
-		results = model_dict[model_name](img_batch.copy(), size = img_size)
+		#.copy() because the images are modified when running model, and we need originals when drawing bboxes later
+		results = model_dict[model_name](img_batch.copy(), size = img_size) 
 		json_results = results_to_json(results,model_dict[model_name])
 
 		for idx, (img, bbox_list) in enumerate(zip(img_batch, json_results)):
 			for bbox in bbox_list:
 				label = f'{bbox["class_name"]} {bbox["confidence"]:.2f}'
-				plot_one_box(bbox['normalized_box'], img, label=label, 
+				plot_one_box(bbox['bbox'], img, label=label, 
 						color=colors[int(bbox['class'])], line_thickness=3)
 
-			#base64 encode the image so we can render it in HTML
-			buffered = BytesIO()
-			img.save(buffered, format="JPEG")
-
-			payload = {'image_base64':base64.b64encode(buffered.getvalue()).decode('utf-8'),
-						'width': img.size[0],
-						'height': img.size[1]}
+			payload = {'image_base64':base64EncodeImage(img)}
 			json_results[idx].append(payload)
 
 	else:
@@ -232,16 +136,47 @@ async def detect_via_api(request: Request,
 
 	return json_results
 	
+##############################################
+#--------------Helper Functions---------------
+##############################################
 
-@app.get("/about/")
-def about_us(request: Request):
+def results_to_json(results, model):
+	''' Converts yolo model output to json (list of list of dicts)
 	'''
-	Display about us page
-	'''
+	return [
+				[
+					{
+					"class": int(pred[5]),
+					"class_name": model.model.names[int(pred[5])],
+					"bbox": [int(x) for x in pred[:4].tolist()], #convert bbox results to int from float
+					"confidence": float(pred[4]),
+					}
+				for pred in result
+				]
+			for result in results.xyxy
+			]
 
-	return templates.TemplateResponse('about.html', 
-			{"request": request})
 
+def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
+	# Directly copied from: https://github.com/ultralytics/yolov5/blob/cd540d8625bba8a05329ede3522046ee53eb349d/utils/plots.py
+    # Plots one bounding box on image 'im' using OpenCV
+    assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
+    tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+def base64EncodeImage(img):
+	_, im_arr = cv2.imencode('.jpg', img)
+	im_b64 = base64.b64encode(im_arr.tobytes()).decode('utf-8')
+
+	return im_b64
 
 if __name__ == '__main__':
 	import uvicorn
@@ -255,6 +190,5 @@ if __name__ == '__main__':
 		model_dict = {model_name: torch.hub.load('ultralytics/yolov5', model_name, pretrained=True) 
 						for model_name in model_selection_options}
 	
-	#make the app string equal to whatever the name of this file is
-	app_str = 'server:app'
+	app_str = 'server:app' #make the app string equal to whatever the name of this file is
 	uvicorn.run(app_str, host='localhost', port=8000, reload=True, workers=1)

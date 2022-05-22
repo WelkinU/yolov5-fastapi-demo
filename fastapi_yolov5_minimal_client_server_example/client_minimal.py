7849336a43fa2a37e6dbaeff0d6c20ba15487680
WelkinU <welkinuttaro@gmail.com>
2022-05-21 21:57:06 -0400

    Converting tabs to spaces

--- fastapi_yolov5_minimal_client_server_example/client_minimal.py
+++ fastapi_yolov5_minimal_client_server_example/client_minimal.py
@@ -6,12 +6,12 @@ import json
 from pprint import pprint
 
 def send_request(image = '../images/zidane.jpg', model_name = 'yolov5s'):
-	res = r.post("http://localhost:8000", 
-					data={'model_name': model_name}, 
-					files = {'file': open(image , "rb")} #pass the files here
-					)
+    res = r.post("http://localhost:8000", 
+                    data={'model_name': model_name}, 
+                    files = {'file': open(image , "rb")} #pass the files here
+                    )
 
-	pprint(json.loads(res.text))
+    pprint(json.loads(res.text))
 
 if __name__ == '__main__':
-	send_request()
+    send_request()
